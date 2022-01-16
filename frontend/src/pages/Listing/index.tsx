d85139c5bf9fbbf9bf66edf547a871c3ec057af3
wagnersistemalima <wagner.sistemalima@gmail.com>
2022-01-16 16:50:53 -0300

    useState, useEffet

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,20 +1,29 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import {BASE_URL} from 'utils/request';
+import {MoviePage} from 'types/movie';
+
 
 function Listing() {
 
-    // forma errada
+    const [pageNumber, setPageNumber] = useState(0);
 
-    axios.get(`${BASE_URL}/movies`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
     .then(response => {
-        console.log(response.data)
-
+        const data = response.data as MoviePage;
+        console.log(data)
+        setPageNumber(data.number)
+        
     });
+
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
