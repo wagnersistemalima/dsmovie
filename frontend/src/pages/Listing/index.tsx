90b56f7dfff35e6aa9152344dbac683662a4575d
wagnersistemalima <wagner.sistemalima@gmail.com>
2022-01-16 16:09:09 -0300

    fist request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import {BASE_URL} from 'utils/request';
 
 function Listing() {
+
+    // forma errada
+
+    axios.get(`${BASE_URL}/movies`)
+    .then(response => {
+        console.log(response.data)
+
+    });
+
     return (
         <>
             <Pagination />
