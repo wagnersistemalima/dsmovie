60ee01351278e23e4227af3cc1eb9563fdd845c5
wagnersistemalima <wagner.sistemalima@gmail.com>
2022-01-16 21:02:02 -0300

    navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
       <nav className='container'>
         <div className='dsmovie-nav-content'>
-          <h1>CriptoMoedas</h1>
+          <h1>DSMovie</h1>
           <a href="https://github.com/wagnersistemalima">
             <div className='dsmovie-contact-container'>
               <Githubicon />
